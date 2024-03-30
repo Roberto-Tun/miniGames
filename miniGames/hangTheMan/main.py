f6ed49ffec32ca9d71f37d51f64e0004bb625e08
Roberto Tun <rtun2004@gmail.com>
2024-03-30 15:11:56 -0600

    added word file

--- miniGames/hangTheMan/main.py
+++ miniGames/hangTheMan/main.py
@@ -3,3 +3,11 @@ Author: Roberto Tun
 Date: 29/03/24
 Description: Hang the man CLI game that will get the word imported and read from a file
 '''
+# opening the words.txt file
+words = open('words.txt','r')
+
+# adding all the words in the file into wordslist
+wordslist = words.readlines()
+
+for word in wordslist:
+    print(word)
