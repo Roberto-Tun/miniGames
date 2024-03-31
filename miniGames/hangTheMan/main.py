6dbf5c5ee8c2c1d12a4d33484d989ca14399a750
Roberto Tun <rtun2004@gmail.com>
2024-03-30 22:25:24 -0600

    semi working code for hang the man

--- miniGames/hangTheMan/main.py
+++ miniGames/hangTheMan/main.py
@@ -3,11 +3,143 @@ Author: Roberto Tun
 Date: 29/03/24
 Description: Hang the man CLI game that will get the word imported and read from a file
 '''
+# importing random module
+import random
+
 # opening the words.txt file
-words = open('words.txt','r')
+words = open('miniGames/hangTheMan/words.txt','r')
 
 # adding all the words in the file into wordslist
 wordslist = words.readlines()
 
+# creating a list to store the new words without the '\n' character
+listOfWords = []
+
+emptyWordList = []
+
+# using a for the remove the '\n' character of each word and appending it to a list
 for word in wordslist:
-    print(word)
+    listOfWords.append(word.strip("\n"))
+
+# using the random module to get a random word from the list for the user to guess
+def randomWord(wordlist):
+    mystoryWord = random.choice(wordlist)
+    return mystoryWord
+
+def mystoryWordLength(word):
+    for letter in word:
+        emptyWordList.append("-")
+
+def checkLetter(letter, word):
+    for i in range(len(word)):
+        character = word[i]
+        if character == letter:
+            emptyWordList[i] = letter
+
+def checklives(letter, word, lives):
+    if letter not in word:
+        lives = int(lives)- 1
+        return lives
+
+
+def checkWin():
+    if "-" not in emptyWordList:
+        return True
+
+def display_hangman(lives): #function that creates the state of the hangman
+    if lives == 6:
+        print("|------|   ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|______    ")
+    elif lives == 5:
+        print("|------|   ")
+        print("|      O   ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|______    ")
+    elif lives == 4:
+        print("|------|   ")
+        print("|      O   ")
+        print("|      |   ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|______    ")
+    elif lives == 3:
+        print("|------|   ")
+        print("|      O   ")
+        print("|     /|   ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|______    ")
+    elif lives == 2:
+        print("|------|   ")
+        print("|      O   ")
+        print("|     /|\  ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|______    ")
+    elif lives == 1:
+        print("|------|   ")
+        print("|      O   ")
+        print("|     /|\  ")
+        print("|     /    ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|______    ")
+    elif lives == 0:
+        print("|------|   ")
+        print("|      O   ")
+        print("|     /|\  ")
+        print("|     / \  ")
+        print("|          ")
+        print("|          ")
+        print("|          ")
+        print("|______    ")
+
+def hangman():
+    gameOver = False
+    mystoryWord = randomWord(listOfWords).upper()
+    mystoryWordLength(mystoryWord)
+    lives = 6
+
+    print("------------------")
+    print("Welcome to Hangman")
+    print("------------------")
+    print("\n")
+
+    while gameOver != True:
+        hidden_string = " ".join(emptyWordList)
+        print(mystoryWord)
+        display_hangman(lives)
+        print("Current lives: ",  lives)
+        print("The Word to guess is: ", hidden_string)
+        guess = input("Enter you letter guess: ").upper()
+        checkLetter(guess, mystoryWord)
+        lives = checklives(guess,mystoryWord, lives)
+        print(emptyWordList)
+        if lives <= 0:
+            print("The word was: ", mystoryWord)
+            gameOver = True
+
+
+        if checkWin() == True:
+            print("Great Job you Won!")
+            gameOver = True
+
+        
+hangman()
