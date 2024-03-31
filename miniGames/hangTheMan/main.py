c7a4aa991b2ad082ea09d72a178b6bf528ad9610
Roberto Tun <rtun2004@gmail.com>
2024-03-31 00:09:39 -0600

    completed hangman code

--- miniGames/hangTheMan/main.py
+++ miniGames/hangTheMan/main.py
@@ -15,6 +15,7 @@ wordslist = words.readlines()
 # creating a list to store the new words without the '\n' character
 listOfWords = []
 
+# creating an empty list to store the character of every letter in the word
 emptyWordList = []
 
 # using a for the remove the '\n' character of each word and appending it to a list
@@ -26,25 +27,27 @@ def randomWord(wordlist):
     mystoryWord = random.choice(wordlist)
     return mystoryWord
 
+# created a function to fill the empty list with "-" for every letter in the word
 def mystoryWordLength(word):
     for letter in word:
         emptyWordList.append("-")
 
+# created a function to check if the letter is in the word and return the lives depending if the letter is or is not in the word
 def checkLetter(letter, word, lives):
     if letter in word:
-        for i in range(len(word)):
-            if word[i] == letter:
-                emptyWordList[i] = letter
+        for position in range(len(word)): # for used to iterate through the word 
+            if word[position] == letter: # if checks that checks if the current letter matches the letter guessed
+                emptyWordList[position] = letter 
         return lives
     else:
         return lives-1
 
-
+# function that checks if the list no longer has "-" which means the word is complete
 def checkWin():
     if "-" not in emptyWordList:
         return True
 
-def display_hangman(lives): #function that creates the state of the hangman
+def display_hangman(lives): # function that creates the state of the hangman
     if lives == 6:
         print("|------|   ")
         print("|          ")
@@ -109,10 +112,12 @@ def display_hangman(lives): #function that creates the state of the hangman
         print("|          ")
         print("|______    ")
 
+# main function to run the game
 def hangman():
+    # local variables
     gameOver = False
-    mystoryWord = randomWord(listOfWords).upper()
-    mystoryWordLength(mystoryWord)
+    mystoryWord = randomWord(listOfWords).upper() # getting the random word 
+    mystoryWordLength(mystoryWord) # creating the empty list with the size of the word
     lives = 6
 
     print("------------------")
@@ -121,14 +126,12 @@ def hangman():
     print("\n")
 
     while gameOver != True:
-        hidden_string = " ".join(emptyWordList)
-        print(mystoryWord)
-        display_hangman(lives)
-        print("Current lives: ",  lives)
+        hidden_string = " ".join(emptyWordList) # converting the empty list to a str for better presentation
+        display_hangman(lives) # displaying the man current state
+        print("Current lives: ",  lives) 
         print("The Word to guess is: ", hidden_string)
         guess = input("Enter you letter guess: ").upper()
         lives = checkLetter(guess, mystoryWord, lives)
-        print(emptyWordList)
         if lives < 0:
             print("The word was: ", mystoryWord)
             gameOver = True
