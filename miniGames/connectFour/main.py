67f2666b1a73e67eba0a5402ce40fdb9e9bb789f
Roberto Tun <rtun2004@gmail.com>
2024-04-06 16:04:08 -0600

    Connect four working

--- miniGames/connectFour/main.py
+++ miniGames/connectFour/main.py
@@ -13,73 +13,91 @@ import numpy as np
 
 board = np.zeros((6, 7))
 
+def show_board(board):
+    print("\n")
+    print(" |1||2||3||4||5||6||7|")
+    print(board)
+
 def update_board(board, placement, currentPlayer):
-    rowUsed = 0
     column = board[:,(placement-1)] #acces to the column trying to update
     for row in reversed(range(len(column))): #iterating through the column
         if column[row] == 0: 
             column[row] = currentPlayer
-            rowUsed = row
             break
     board[:, placement-1] = column
-    return board, rowUsed
+    return board
 
 def checkWinner(board, currentPlayer):
     # checking for horizontal wins
-    for row in range(len(board)):
+    for row in range(6):
         for column in range(4):
-            if (board[row][column] == currentPlayer and board[row][column+1]== currentPlayer and board[row][column+2]== currentPlayer and board[row][column+3]== currentPlayer):
-                return "Winner"
+            if board[row][column] == currentPlayer and board[row][column+1] == currentPlayer and board[row][column+2] == currentPlayer and board[row][column+3] == currentPlayer:
+                return currentPlayer
     # checking for vertical wins
-    for row in range(len(board)):
-        for column in range(4):
-            if (board[row][column] == currentPlayer and board[row+1][column]== currentPlayer and board[row+2][column]== currentPlayer and board[row+3][column]== currentPlayer):
-                return "Winner"
-    # checking for diagonal wins
-    for row in range(len(board)-3):
+    for column in range(7):
+        for row in range(3):
+            if board[row][column] == currentPlayer and board[row+1][column] == currentPlayer and board[row+2][column] == currentPlayer and board[row+3][column] == currentPlayer:
+                return currentPlayer
+    #checking diagonal wins
+    for row in range(3):
         for column in range(3, 7):
-            if (board[row][column] == currentPlayer and board[row-1][column+1]== currentPlayer and board[row-2][column+2]== currentPlayer and board[row-3][column+3]== currentPlayer):
-                return "Winner"
-    # checking for diagonal wins
-    for row in range(len(board)-3):
+            if board[row][column] == currentPlayer and board[row+1][column-1] == currentPlayer and board[row+2][column-2] == currentPlayer and board[row+3][column-3] == currentPlayer:
+                return currentPlayer
+    #checking diagonal wins
+    for row in range(3):
         for column in range(4):
-            if (board[row][column] == currentPlayer and board[row+1][column+1]== currentPlayer and board[row+2][column+2]== currentPlayer and board[row+3][column+3]== currentPlayer):
-                return "Winner"
+            if board[row][column] == currentPlayer and board[row+1][column+1] == currentPlayer and board[row+2][column+2] == currentPlayer and board[row+3][column+3] == currentPlayer:
+                return currentPlayer
+
 
 def conncetFour():
     board = np.zeros((6, 7))
     gameOver = False
     currentPlayer = 1
-    win = ""
+    playerOneTurns = 21
+    playerTwoTurns = 21
  
     print("-----------------------")
     print("Welcome to Connect Four")
     print("-----------------------")
-    print("\n")
-    print(board)
+    show_board(board)
 
     while gameOver != True:
-        if currentPlayer == 1:
+        if currentPlayer == 1 and playerOneTurns > 0:
             print("Player X Turn: ")
             column = int(input("Enter a number between 1 - 7: "))
-            board, row = update_board(board, column, currentPlayer)
-            print(row)
-            print(board)
+            columnCheck = board[:,(column-1)]
+            if 0 not in columnCheck:
+                print("Position is full")
+            elif column >= 1 and column <= 7:
+                board = update_board(board, column, currentPlayer)
+            show_board(board)
             win = checkWinner(board, currentPlayer)
             currentPlayer = 2
-        elif currentPlayer == 2:
+            playerOneTurns -= 1
+        elif currentPlayer == 2 and playerTwoTurns > 0:
             print("Player Y Turn: ")
             column = int(input("Enter a number between 1 - 7: "))
-            board, row = update_board(board, column, currentPlayer)
-            print(board)
-            print(row)
+            columnCheck = board[:,(column-1)]
+            if 0 not in columnCheck:
+                print("Position is full")
+            elif column >= 1 and column <= 7:
+                board = update_board(board, column, currentPlayer)
+            show_board(board)
             win = checkWinner(board, currentPlayer)
             currentPlayer = 1
-
-        if win == "Winner":
-            print("The Winner is: ", currentPlayer)
+            playerTwoTurns -= 1
+        else:
+            print("Its a tie")
             gameOver = True
 
+        if win == 1:
+            print("The Winner is: Player One")
+            gameOver = True
+        elif win == 2:
+            print("The Winner is Player Two")
+            gameOver = True
+        
+
+
 conncetFour()
-
-
